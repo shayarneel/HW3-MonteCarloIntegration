5a8a9b6d20fd23b2ed7e8d0a6d55294e21822189
Jeff Terry <terryj@iit.edu>
2021-01-01 17:06:12 -0600

    SwiftUI version of the Monte Carlo Integration program to determine the value of PI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pi = 0.0
+    @State var totalGuesses = 0.0
+    @State var totalIntegral = 0.0
+    @State var radius = 1.0
+    @State var guessString = "23458"
+    @State var totalGuessString = "0"
+    @State var piString = "0.0"
+    
+    
+    // Setup the GUI to monitor the data from the Monte Carlo Integral Calculator
+    @ObservedObject var monteCarlo = MonteCarloCircle(withData: true)
+    
+    //Setup the GUI View
     var body: some View {
-        Text("Hello, world!")
+        HStack{
+            
+            VStack{
+                
+                VStack(alignment: .center) {
+                    Text("Guesses")
+                        .font(.callout)
+                        .bold()
+                    TextField("# Guesses", text: $guessString)
+                        .padding()
+                }
+                .padding(.top, 5.0)
+                
+                VStack(alignment: .center) {
+                    Text("Total Guesses")
+                        .font(.callout)
+                        .bold()
+                    TextField("# Total Guesses", text: $totalGuessString)
+                        .padding()
+                }
+                
+                VStack(alignment: .center) {
+                    Text("π")
+                        .font(.callout)
+                        .bold()
+                    TextField("# π", text: $piString)
+                        .padding()
+                }
+                
+                Button("Cycle Calculation", action: {self.calculatePi()})
+                    .padding()
+                
+                Button("Clear", action: {self.clear()})
+                    .padding(.bottom, 5.0)
+                
+                
+            }
             .padding()
+            
+            //DrawingField
+            
+            
+            drawingView(redLayer:$monteCarlo.insideData, blueLayer: $monteCarlo.outsideData)
+                .padding()
+                .aspectRatio(1, contentMode: .fit)
+            // Stop the window shrinking to zero.
+            Spacer()
+            
+        }
     }
+    
+    func calculatePi(){
+        
+        monteCarlo.guesses = Int(guessString)!
+        monteCarlo.radius = radius
+        monteCarlo.totalGuesses = Int(totalGuessString) ?? Int(0.0)
+        
+        monteCarlo.calculatePI()
+        
+        totalGuessString = monteCarlo.totalGuessesString
+        
+        piString =  monteCarlo.piString
+        
+        
+        
+    }
+    
+    func clear(){
+        
+        guessString = "23458"
+        totalGuessString = "0.0"
+        piString =  ""
+        monteCarlo.totalGuesses = 0
+        monteCarlo.totalIntegral = 0.0
+        monteCarlo.insideData = []
+        monteCarlo.outsideData = []
+        
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +110,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
--- Tests macOS/Tests_macOS.swift
+++ Tests macOS/Tests_macOS.swift
@@ -30,6 +30,57 @@ class Tests_macOS: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testBoundingBoxVolume() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // Given
+        let myBoundingBox = BoundingBox()
+        let length = 1.0
+        let width = 2.2
+        let height = 4.3
+        
+        let volume = myBoundingBox.calculateVolume(lengthOfSide1: length, lengthOfSide2: width, lengthOfSide3: height)
+        
+        // Then
+        XCTAssertEqual(volume, 9.46, accuracy: 0.000000001, "expected better from you")
+    }
+    
+    func testBoundingBoxSurfaceArea() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // Given
+        let myBoundingBox = BoundingBox()
+        let length = 1.0
+        let width = 2.2
+        let height = 4.3
+        
+        let surfaceArea = myBoundingBox.calculateSurfaceArea(numberOfSides: 6, lengthOfSide1: length, lengthOfSide2: width, lengthOfSide3: height)
+        // Then
+        XCTAssertEqual(surfaceArea, 31.92, accuracy: 0.000000001, "expected better from you")
+    }
+    
+    func testMonteCarloIntegral() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let monteCarlo = MonteCarloCircle(withData: true)
+        
+        monteCarlo.guesses = 9120217
+        monteCarlo.calculatePI()
+        
+        let ourPI = Double(monteCarlo.piString)
+        
+        // Then
+        XCTAssertEqual(ourPI!, Double.pi, accuracy: 0.005, "expected better from you")
+        
+        let totalGuesses = Double(monteCarlo.totalGuesses)
+        XCTAssertEqual(totalGuesses, 9120217, accuracy: 0.005, "expected better from you")
+        
+    }
+
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
